1bdda05a9b7e7bcb1a1d230dc7102cc6c0bf1e89
Jens <dpluotao@dfl.com.cn>
2017-11-21 16:57:39 +0800

    [add/compose里配置时区]

--- src/main/java/com/jens/springbootmodel/common/ValidationConstant.java
+++ src/main/java/com/jens/springbootmodel/common/ValidationConstant.java
@@ -12,4 +12,21 @@ public class ValidationConstant {
 
     public final static String VALIDATION_SUCCESS="校验成功";
 
+    /**
+     * 校验规则--不能为空
+     */
+
+
+    /**
+     * 校验规则--正则
+     */
+    public final static String LICENSEPLATENUMBER_WRONG = "车牌号格式错误,正确格式举例[鄂A12345]";
+
+    /**
+     * 校验规则--其他
+     */
+    public final static String POSITIVE_INTEGER = "必须是正整数";
+    public final static String POSITIVE_0_1 = "只能是0或1";
+
+
 }
